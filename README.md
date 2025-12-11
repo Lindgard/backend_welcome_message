58d8ed536981c994a5e5630efe046624916cec19
ALP <a.b.lindgard@gmail.com>
2025-12-11 10:39:13 +0100

    added nix flake and started README

---
+++ README.md
@@ -0,0 +1,35 @@
+# backend_welcome_message
+
+## Description
+
+_Fill in a short project description here._
+
+## Dev environment (Nix flake)
+
+- Prerequisite: Nix with flakes enabled.
+- Enter the shell:
+
+```sh
+nix develop
+```
+
+- Run the app:
+
+```sh
+dotnet run
+```
+
+- Or run via flake app:
+
+```sh
+nix run
+```
+
+## Without Nix
+
+- Install .NET SDK 8.
+- Run:
+
+```sh
+dotnet run
+```
---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Nix flake for backend_welcome_message (.NET console app)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+        };
+
+        dotnet = pkgs.dotnet-sdk_8;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            dotnet
+            pkgs.git
+          ];
+
+          # Make dotnet CLI happy inside Nix
+          DOTNET_ROOT = "${dotnet}";
+          DOTNET_CLI_TELEMETRY_OPTOUT = "1";
+        };
+
+        apps.default = {
+          type = "app";
+          program =
+            let runner = pkgs.writeShellApplication {
+              name = "run-backend-welcome-message";
+              runtimeInputs = [ dotnet ];
+              text = ''
+                set -euo pipefail
+                dotnet run --project "${self}/backend_welcome_message.csproj" "$@"
+              '';
+            };
+            in "${runner}/bin/run-backend-welcome-message";
+        };
+      });
+}
