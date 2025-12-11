4536ea099341987654022e25db7d00a010717664
ALP <a.b.lindgard@gmail.com>
2025-12-11 11:54:36 +0100

    added formatting of the date and time, built a switch for the times of day with messages

--- Program.cs
+++ Program.cs
@@ -12,4 +12,26 @@ class Program
         var greeting = BuildGreeting(now);
         Console.WriteLine(greeting);
     }
+
+    static string BuildGreeting(DateTime now)
+    {
+        var formatted = now.ToString("dddd dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture);
+        string timeOfDay;
+
+       switch (now.Hour)
+       {
+        case >= 5 and <= 12:
+            timeOfDay = "Good morning";
+            break;
+        case >= 12 and <= 18:
+            timeOfDay = "Good afternoon";
+            break;
+        case >= 18 and <= 22:
+            timeOfDay = "Good evening";
+            break;
+        default:
+            timeOfDay = "Good night";
+            break;
+       }
+    }
 }
